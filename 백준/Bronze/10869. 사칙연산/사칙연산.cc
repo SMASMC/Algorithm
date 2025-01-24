83db1ea1eb00cee59f0f08952281a287ea6440c1
송명철 <95916983+SMASMC@users.noreply.github.com>
2025-01-24 16:32:53 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a=0;
+    int b=0;
+    
+    scanf("%d",&a);
+    scanf("%d",&b);
+    
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+    
+    return 0;
+}
