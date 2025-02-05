14dc04e82bd273fb65d1ff7b7ec855788ee35e4f
송명철 <95916983+SMASMC@users.noreply.github.com>
2025-02-06 01:37:09 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,13 +1,17 @@
-rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
-grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+grade = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0','D+','D0','F']
+score = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0]
 
-total = 0	# 학점 총합을 담을 변수
-result = 0	# (학점 * 과목평점) 총합을 담을 변수
-for _ in range(20) :
-    s, p, g = input().split()
-    p = float(p)
-    if g != 'P' :	# 등급이 P인 과목은 계산 안함
-        total += p
-        result += p * grade[rating.index(g)]
+total = 0
+result_avg=0
 
-print('%.6f' % (result / total))
+for i in range(20):
+    sub, sco, gra=input().split()
+    if gra in grade:
+        if gra == 'P':
+            continue
+        total+=float(sco)
+        result_avg+=float(sco)*score[grade.index(gra)]
+        
+map_score_grade=(result_avg/total)
+
+print(round(map_score_grade,6))
