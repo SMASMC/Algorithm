460a7ef3cdcb08ee2053f11ef2b9206a589ffe40
송명철 <95916983+SMASMC@users.noreply.github.com>
2025-02-10 22:37:13 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,10 @@
+# 2738번 행렬 덧셈
+N, M = map(int, input().split())
+
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+
+for n in range(N):
+    for m in range(M):
+        print(A[n][m] + B[n][m], end = ' ')
+    print()
