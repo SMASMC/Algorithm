5ca280f39b0e2c3840c8c1d0898d8eeb86a416fd
송명철 <95916983+SMASMC@users.noreply.github.com>
2025-02-13 00:16:36 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,6 +1,11 @@
-words = [input() for i in range(5)]
+board = [list(input()) for _ in range(5)]
 
-for j in range(15):
-    for i in range(5):
-        if j < len(words[i]):
-            print(words[i][j], end='')
+new_word=''
+max_idx=max(len(board[row]) for row in range(5)) # 가장 많은 index => col을 지닌 값을 찾기
+
+for col in range(max_idx):
+    for row in range(5):
+        if col < len(board[row]): # 최대 크기를 벗어나지 않는다면.
+            new_word +=board[row][col]
+
+print(new_word)
