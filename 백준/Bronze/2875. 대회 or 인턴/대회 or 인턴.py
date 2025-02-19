8ff6034e51011362bff3bada5665f1b0f24eee58
송명철 <95916983+SMASMC@users.noreply.github.com>
2025-02-19 12:46:22 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -1,13 +1,11 @@
-# 2875번 대회 or 인턴
-# 여2, 남1 으로 구성된 팀 1
-N, M, K = map(int, input().split()) # N 여, M 남, K 참여해야하는 학생 수
-team_count = 0
-woman = N
-man = M
-while (woman + man - K >= 3) and (woman >=2 and man >=1): # 팀으로된 인원수가 인턴으로 빠진 인원보다 작을 경우와 여,남이 다 있을 경우까지
-    woman -=2
-    man -=1
-    if woman >= 0 and man >= 0: # 마지막 인원까지 team으로 count한다.
-        team_count+=1
+# 2875 번 대회 or 인턴
 
+N, M, K = map(int,input().split()) # N 여학생, M 남학생, K 인턴쉽
+
+team_count = 0
+while (N + M - K >= 3) and (N>=2 and M>=1):
+    N-=2
+    M-=1
+    team_count+=1
+    
 print(team_count)
