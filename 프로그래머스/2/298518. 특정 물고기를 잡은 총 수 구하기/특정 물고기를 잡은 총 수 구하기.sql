fcfd9ebd3ab1e5c4f206d08814bfa1f8daca29f5
SMASMC <95916983+SMASMC@users.noreply.github.com>
2025-03-26 01:11:23 +0900

    [level 2] Title: 특정 물고기를 잡은 총 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
@@ -0,0 +1,4 @@
+select count(info.id) as fish_count
+from fish_info as info inner join fish_name_info as name_info
+on info.fish_type = name_info.fish_type
+where name_info.fish_name = 'BASS' or  name_info.fish_name = 'SNAPPER' 
