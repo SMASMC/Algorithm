34972d37d7e8d4e1db8b5631812f9346a0c65049
SMASMC <95916983+SMASMC@users.noreply.github.com>
2025-03-26 15:58:40 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,9 @@
+# 15 : 28
+
+select id, email, first_name, last_name
+from DEVELOPERS
+where
+skill_code & (select code from SKILLCODES where name = 'Python')
+or 
+skill_code & (select code from SKILLCODES where name = 'C#')
+order by id asc
