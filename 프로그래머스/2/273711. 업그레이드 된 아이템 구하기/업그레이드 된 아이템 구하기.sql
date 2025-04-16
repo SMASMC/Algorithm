0e3b2b435ee1c30bc0761e4fb03c3bb736039bcf
SMASMC <95916983+SMASMC@users.noreply.github.com>
2025-04-16 21:47:09 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,9 +1,12 @@
-# 20 : 42
-select item_id, item_name, rarity
-from item_info
-where item_id in (select tree.item_id from item_info info,item_tree tree 
-                  where info.item_id = tree.parent_item_id 
-                  and info.rarity = 'rare')
+# 21 : 11
+# 아이템의 희귀도가 RARE인 아이템들의 모든 다음 업그레이드(계층 구조) 아이템의 정보를 출력
+# 결과는 아이템 ID를 기준으로 내림차순 정렬
+
+select info.item_id as item_id, info.item_name as item_name, info.RARITY as RARITY
+from item_tree as tree 
+# 자식 아이템의 정보와 tree와 맞는지 확인
+join item_info as info on tree.item_id = info.item_id
+# 부모 아이템 정보가 tree에 있는지 확인
+join item_info as info2 on tree.parent_item_id = info2.item_id
+where info2.rarity = 'RARE'
 order by item_id desc
-# parent_item_id를 따라 찾아서 올라간 다음에 최상위(root)에 도달하면, 그만 찾는 것으로 해야함
-# tree같은 느낌으로 찾아 올라가야함.
